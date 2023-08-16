c75a52d265e0a2bf00f06450bc6af09ce36cf2b6
ejulianops <135237370+ejulianops@users.noreply.github.com>
2023-08-16 22:23:44 +0100

    part 11, exercise 06 completed

---
+++ part11-06_begin_with_vowel/src/begin_with_vowel.py
@@ -0,0 +1,13 @@
+# Please write a function named begin_with_vowel(words: list) which takes a list of strings as its argument.
+# The function should use a list comprehension technique to create and return a new list, 
+# containing only those words from the original list which begin with a vowel (a, e, i, o, u). 
+# Both lowercase and uppercase letters should be accepted.
+# The maximum length of the function is two lines of code, including the header line beginning with the def keyword.
+
+def begin_with_vowel(words: list):
+    return [word for word in words if word[0] in 'aeiouAEIOU']    
+
+if __name__ == '__main__':
+    word_list = ["automobile","motorbike","Animal","cat","Dog","APPLE","orange"]
+    for vowelled in begin_with_vowel(word_list):
+        print(vowelled)
