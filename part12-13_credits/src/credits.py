bd35874cddb23f09e8484488f786279abe756d16
ejulianops <135237370+ejulianops@users.noreply.github.com>
2023-09-06 22:24:56 +0100

    part 12, exercise 12, part 2/3 completed

--- part12-13_credits/src/credits.py
+++ part12-13_credits/src/credits.py
@@ -11,11 +11,24 @@ class CourseAttempt:
 
 # Write your solution
 
+# part 1
+
 def credits_sum_helper(balance_sum, attempt):
     return balance_sum + attempt.credits
 
 def sum_of_all_credits(course_attempts):
     return reduce(credits_sum_helper, course_attempts, 0)
+
+# part 2
+
+def sum_passed_credits(balance_sum, attempt):
+    if attempt.grade > 0:
+        return balance_sum + attempt.credits
+    else:
+        return balance_sum
+
+def sum_of_passed_credits(course_attempts):
+    return reduce(sum_passed_credits, course_attempts, 0)
 
 if __name__ == "__main__":
 
@@ -26,4 +39,9 @@ if __name__ == "__main__":
     credit_sum = sum_of_all_credits([s1, s2, s3])
     print(credit_sum)
 
-    
+    print('\n--- Part 2 ---\n')
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_passed_credits([s1, s2, s3])
+    print(credit_sum)
