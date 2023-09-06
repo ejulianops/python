f479c6c071706f249916784777651e3129caee08
ejulianops <135237370+ejulianops@users.noreply.github.com>
2023-09-06 23:29:24 +0100

    part 12, exercise 13 completed

--- part12-13_credits/src/credits.py
+++ part12-13_credits/src/credits.py
@@ -21,16 +21,19 @@ def sum_of_all_credits(course_attempts):
 
 # part 2
 
-def sum_passed_credits(balance_sum, attempt):
-    if attempt.grade > 0:
-        return balance_sum + attempt.credits
-    else:
-        return balance_sum
-
 def sum_of_passed_credits(course_attempts):
-    return reduce(sum_passed_credits, course_attempts, 0)
+    sum_passed_credits = list(filter(lambda attempt : attempt.grade > 0, course_attempts))
 
-if __name__ == "__main__":
+    return reduce(lambda credits, attempt : credits + attempt.credits, sum_passed_credits, 0)
+
+# part 3
+
+def average(course_attempts: list):
+    passed_attempts = list(filter(lambda attempt : attempt.grade > 0, course_attempts))
+
+    return reduce(lambda grades, attempt: grades + attempt.grade, passed_attempts, 0) / len(passed_attempts)
+
+if __name__ == "__main__":cd ..
 
     print('\n--- Part 1 ---\n')
     s1 = CourseAttempt("Introduction to Programming", 5, 5)
@@ -45,3 +48,10 @@ if __name__ == "__main__":
     s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
     credit_sum = sum_of_passed_credits([s1, s2, s3])
     print(credit_sum)
+
+    print('\n--- Part 3 ---\n')
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 0, 4)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    ag = average([s1, s2, s3])
+    print(ag)
