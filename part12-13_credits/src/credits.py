4200ead0a37732f372bc6b02e64bba31bb2f038b
ejulianops <135237370+ejulianops@users.noreply.github.com>
2023-09-06 22:12:39 +0100

    part 12, exercise 12, part 1/3 completed

---
+++ part12-13_credits/src/credits.py
@@ -0,0 +1,29 @@
+from functools import reduce
+
+class CourseAttempt:
+    def __init__(self, course_name: str, grade: int, credits: int):
+        self.course_name = course_name
+        self.grade = grade
+        self.credits = credits
+
+    def __str__(self):
+        return f"{self.course_name} ({self.credits} cr) grade {self.grade}"
+
+# Write your solution
+
+def credits_sum_helper(balance_sum, attempt):
+    return balance_sum + attempt.credits
+
+def sum_of_all_credits(course_attempts):
+    return reduce(credits_sum_helper, course_attempts, 0)
+
+if __name__ == "__main__":
+
+    print('\n--- Part 1 ---\n')
+    s1 = CourseAttempt("Introduction to Programming", 5, 5)
+    s2 = CourseAttempt("Advanced Course in Programming", 4, 5)
+    s3 = CourseAttempt("Data Structures and Algorithms", 3, 10)
+    credit_sum = sum_of_all_credits([s1, s2, s3])
+    print(credit_sum)
+
+    
